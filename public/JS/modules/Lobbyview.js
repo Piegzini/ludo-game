3eb457c2114a9fd0a1f551b6f30b0160d11f07fe
Konrad Piegza <piegzini.k@gmail.com>
2021-04-21 14:00:26 +0200

    Adding a feature to update players
    I added a function that periodically sends requests for information about the lobby, then based on the response updates the players divs

--- public/JS/modules/Lobbyview.js
+++ public/JS/modules/Lobbyview.js
@@ -2,21 +2,17 @@ export default class Lobbyview {
   constructor(players_information) {
     this.template = document.querySelector('#lobby');
     this.statuses = ['Nie gotowy', 'Gotowy '];
-    this.build(players_information);
+    this.lobbyUpdaterInterval;
+    this.playersContentDivs;
+    this.buildView(players_information);
+
+    this.setLobbyUpdater();
   }
 
-  build(players_information) {
-    const players_data = players_information;
-    const lobby = document.querySelector('#input-wrapper');
-    if (lobby) {
-      lobby.remove();
-    }
-    const templateClone = this.template.content.cloneNode(true);
-    const playersContentsDivs = templateClone.querySelectorAll('.p-content');
-
+  updatePlayersDivs(players_data) {
     for (const [index, player] of players_data.entries()) {
       const { nick, status, color } = player;
-      const playerContentDiv = playersContentsDivs[index];
+      const playerContentDiv = this.playersContentDivs[index];
       const id = `p-${color}`;
 
       playerContentDiv.setAttribute('id', id);
@@ -24,7 +20,38 @@ export default class Lobbyview {
       childsOfContentDiv[0].textContent = nick;
       childsOfContentDiv[1].textContent = this.statuses[status];
     }
+  }
+
+  buildView(players_information) {
+    const players_data = players_information;
+    const lobby = document.querySelector('#input-wrapper');
+    if (lobby) {
+      lobby.remove();
+    }
+    const templateClone = this.template.content.cloneNode(true);
+    this.playersContentDivs = templateClone.querySelectorAll('.p-content');
+    this.updatePlayersDivs(players_data);
 
     document.body.append(templateClone);
+
+    this.playersContentDivs = document.querySelectorAll('.p-content');
+  }
+
+  lobbyUpdater = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/information');
+      if (!response.ok) {
+        throw new Error(`http error: ${response.status}`);
+      }
+
+      const players_data = await response.json();
+      this.updatePlayersDivs(players_data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  setLobbyUpdater() {
+    this.lobbyUpdaterInterval = setInterval(this.lobbyUpdater, 3000);
   }
 }
